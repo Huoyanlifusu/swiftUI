c360aa84d0c0a1d34ecd14486d79254224ecc6ef
Huoyanlifusu <54940667+Huoyanlifusu@users.noreply.github.com>
2023-03-23 01:30:00 +0800

    Update setting.swift
    更新三种UI的设计

--- setting.swift
+++ setting.swift
@@ -1,175 +1,103 @@
-//
-//  SettingSUIV.swift
-//  ScanKit
-//
-//  Created by 张裕阳 on 2023/3/17.
-//
-
-import Foundation
-import SwiftUI
-
-
-struct SettingSUIV: View {
-    @State private var selectedAlgorithm: Algorithm = .lidar
-    @State private var voxelSize: VoxelSize = .twomm
-    
-    @State private var saveData = true
-    
-    @State private var isPresentingAbout = false
-    @State private var isPresentingPrivacy = false
-    
-    @State private var HStackBackgroundColor = Color.white
-    
-    enum Algorithm: String, CaseIterable, Identifiable {
-        case ML, lidar
-        var id: Self { self }
-    }
-    
-    enum VoxelSize: CaseIterable, Identifiable {
-        case twomm, fivemm, tenmm
-        var id: Self { self }
-    }
-    
-    
-    var body: some View {
-        NavigationStack {
-            List {
-                Section(header: Text("Mesh")) {
-                    VStack {
-                        HStack {
-                            Text("Voxel Size")
-                                .multilineTextAlignment(.leading)
-                            Spacer()
-                        }
-                        Rectangle()
-                            .fill(Color(uiColor: UIColor.lightGray))
-                            .frame(maxWidth: .infinity)
-                            .frame(height: 40)
-                            .cornerRadius(15)
-                            .overlay(
-                                HStack {
-                                    RoundedRectangle(cornerRadius: 15)
-                                        .fill((voxelSize == VoxelSize.twomm) ? Color.white : Color(uiColor: UIColor.lightGray))
-                                        .frame(maxWidth: 88)
-                                        .frame(height: 36)
-                                        .overlay {
-                                            Text("2mm")
-                                                .onTapGesture {
-                                                    voxelSize = VoxelSize.twomm
-                                                }
-                                                .padding(5)
-                                        }
-                                    Divider()
-                                        .background(Color.white)
-                                    RoundedRectangle(cornerRadius: 15)
-                                        .fill((voxelSize == VoxelSize.fivemm) ? Color.white : Color(uiColor: UIColor.lightGray))
-                                        .frame(maxWidth: 88)
-                                        .frame(height: 36)
-                                        .overlay {
-                                            Text("5mm")
-                                                .onTapGesture {
-                                                    voxelSize = VoxelSize.fivemm
-                                                }
-                                                .padding(5)
-                                        }
-                                    Divider()
-                                        .background(Color.white)
-                                    RoundedRectangle(cornerRadius: 15)
-                                        .fill((voxelSize == VoxelSize.tenmm) ? Color.white : Color(uiColor: UIColor.lightGray))
-                                        .frame(maxWidth: 88)
-                                        .frame(height: 36)
-                                        .overlay {
-                                            Text("10mm")
-                                                .onTapGesture {
-                                                    voxelSize = VoxelSize.tenmm
-                                                }
-                                                .padding(5)
-                                        }
-                                }
-                            )
-                            .mask(
-                                RoundedRectangle(cornerRadius: 15)
-                                    .frame(width: 300, height: 40)
-                            )
-                    }
-                    Picker("Algorithm", selection: $selectedAlgorithm) {
-                        Text("Machine Learning").tag(Algorithm.ML).frame(maxWidth: .infinity)
-                        Text("Lidar Sensor").tag(Algorithm.lidar).frame(maxWidth: .infinity)
-                    }
-                }
-                
-                Section(header: Text("Data Management")) {
-                    Toggle(isOn: $saveData, label: {
-                        Text("Saving Data")
-                    })
-                    Button("Clear Data") {
-                        clearData()
-                    }.foregroundColor(Color.red)
-                }
-                
-                Section(header: Text("Product")) {
-                    NavigationLink(destination: AboutSUIV()) {
-                        Text("About").foregroundColor(Color.blue)
-                    }
-                    NavigationLink(destination: PrivacySUIV()) {
-                        Text("Privacy policy").foregroundColor(Color.blue)
-                    }
-                    Button("Sending Feedback") {
-                        openMailBox()
-                    }.foregroundColor(Color.blue)
-                }
-                
-                Section(header: Text("Versee")) {
-                    HStack {
-                        Text("Follow on Bilibili")
-                        Spacer()
-                        Image("bilibili")
-                            .resizable()
-                            .frame(maxWidth: 25, maxHeight: 25)
-                    }
-                    .frame(maxWidth: .infinity)
-                    .contentShape(Rectangle())
+//第一种 大方框内嵌套多个小方框的选择框设计
+Rectangle()
+.fill(Color(uiColor: UIColor.lightGray))
+.frame(maxWidth: .infinity)
+.frame(height: 40)
+.cornerRadius(15)
+.overlay(
+    HStack {
+        RoundedRectangle(cornerRadius: 15)
+            .fill((voxelSize == VoxelSize.twomm) ? Color.white : Color(uiColor: UIColor.lightGray))
+            .frame(maxWidth: 88)
+            .frame(height: 36)
+            .overlay {
+                Text("2mm")
                     .onTapGesture {
-                        jumpToBilibili()
+                        voxelSize = VoxelSize.twomm
                     }
-                    HStack {
-                        Text("Follow on Twitter")
-                        Spacer()
-                        Image("Twitter")
-                            .resizable()
-                            .frame(maxWidth: 20, maxHeight: 20)
-                    }
-                    .frame(maxWidth: .infinity, maxHeight: 25)
-                    .contentShape(Rectangle())
-                    .onTapGesture {
-                        jumpToTwitter()
-                    }
-                }
+                    .padding(5)
             }
-        }
+        Divider()
+            .background(Color.white)
+        RoundedRectangle(cornerRadius: 15)
+            .fill((voxelSize == VoxelSize.fivemm) ? Color.white : Color(uiColor: UIColor.lightGray))
+            .frame(maxWidth: 88)
+            .frame(height: 36)
+            .overlay {
+                Text("5mm")
+                    .onTapGesture {
+                        voxelSize = VoxelSize.fivemm
+                    }
+                    .padding(5)
+            }
+        Divider()
+            .background(Color.white)
+        RoundedRectangle(cornerRadius: 15)
+            .fill((voxelSize == VoxelSize.tenmm) ? Color.white : Color(uiColor: UIColor.lightGray))
+            .frame(maxWidth: 88)
+            .frame(height: 36)
+            .overlay {
+                Text("10mm")
+                    .onTapGesture {
+                        voxelSize = VoxelSize.tenmm
+                    }
+                    .padding(5)
+            }
+    }
+)
+.mask(
+    RoundedRectangle(cornerRadius: 15)
+        .frame(width: 300, height: 40)
+)
+
+
+
+//第二种 显示内存占用率的长条UI
+VStack(spacing: 0) {
+    HStack {
+        // https://stackoverflow.com/questions/56465083/custom-font-size-for-text-in-swiftui
+        Text("Used Memory:")
+        Spacer()
+        Text("\(UIDevice.current.usedDiskSpaceInGB)" + " / " + "\(UIDevice.current.totalDiskSpaceInGB)")
+            .font(.system(size: 14)).foregroundColor(Color(UIColor.lightGray))
+    }
+    Spacer().frame(height: 10)
+    GeometryReader { geometry in
+        // https://stackoverflow.com/questions/64031680/remove-padding-bewteen-items-in-hstack
+        HStack(spacing: 0) {
+            Rectangle()
+                .foregroundColor(Color.orange)
+                .frame(width: convertBytesToOccupationRate(usedMemory, totalMemory) * geometry.size.width,
+                       height: geometry.size.height)
+            Rectangle()
+                .foregroundColor(Color.cyan)
+                .frame(width: convertBytesToOccupationRate(freeMemory, totalMemory) * geometry.size.width,
+                       height: geometry.size.height)
+        }.mask(
+            RoundedRectangle(cornerRadius: 15)
+        )
     }
 }
 
-extension SettingSUIV {
-    func clearData() {
-        print("已经删除所有数据！")
-    }
-    
-    func openMailBox() {
-        if let url = URL(string: "message://") {
-            UIApplication.shared.open(url)
-        }
-    }
-    
-    func jumpToBilibili() {
-        if let url = URL(string: "https://space.bilibili.com/1946896308") {
-            UIApplication.shared.open(url)
-        }
-    }
 
-    func jumpToTwitter() {
-        if let url = URL(string: "https://twitter.com/TechVersee") {
-            UIApplication.shared.open(url)
-        }
+//第三种 按钮内嵌套文本和图片 例如一些app的登录/注册按钮
+Button(action: {
+    loginWithWechat()
+}) {
+    HStack {
+        //https://stackoverflow.com/questions/56505692/how-to-resize-image-with-swiftui resize image
+        Image("wechat").resizable().frame(width: 22.9, height: 20)
+        Text("Login With Wechat").bold()
     }
 }
+.foregroundColor(.white)
+.padding()
+.background(Color.black)
+.cornerRadius(.infinity)
+.overlay(content: {
+    RoundedRectangle(cornerRadius: .infinity).stroke(Color.white, lineWidth: 1)
+})
+.frame(width: 300, height: 50)
+
+
+
