5e4f937db340e003cb5587a62995c33f8a273bf2
Huoyanlifusu <54940667+Huoyanlifusu@users.noreply.github.com>
2023-03-23 20:49:52 +0800

    Create 基于Section和ScrollView的UI.swift

---
+++ 基于Section和ScrollView的UI.swift
@@ -0,0 +1,10 @@
+//标题大写居左
+Section(header: HStack {
+                Text("Feature").font(.system(size: 30))
+                    .foregroundColor(.black)
+                    .bold()
+                Spacer()
+            }.padding(.leading, 16)
+            ) {
+                
+            }
