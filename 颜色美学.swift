433840e70e981f5be6915bb0f161a428a2784a8f
Huoyanlifusu <54940667+Huoyanlifusu@users.noreply.github.com>
2023-03-25 04:33:06 +0800

    Update 颜色美学.swift

--- 颜色美学.swift
+++ 颜色美学.swift
@@ -1,5 +1,10 @@
 //最经典的颜色
-Text("hello").foregroundColor(Color.black) //纯黑
+Color.black //纯黑
+Color.white //纯白
 
 //自定义颜色
 let lightGray = Color(red: 0.95, green: 0.95, blue: 0.95)
+
+//前景色和后景色
+UI.forgroundColor()
+UI.background()
